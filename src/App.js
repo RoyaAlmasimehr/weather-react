9e93f3d28019b9ff1039af81d24857bef30f809f
RoyaAlmasimehr <almasimehrroya@gmail.com>
2023-11-23 16:34:01 +0330

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-
 import Weather from "./weather.js";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-  
-        <Weather city="paris" />
-        
+        <Weather />
       </header>
+      <footer>
+        This is project was coded by Roya Almasimehr. and is{" "}
+        <a href="https://github.com/RoyaAlmasimehr/weather-react">
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
     </div>
   );
 }
--- src/weather.js
+++ src/weather.js
@@ -1,28 +1,53 @@
 import React from "react";
-import axios from "axios";
-import { Puff } from "react-loader-spinner";
-export default function Weather (props){
+import "./weather.css";
 
-function handleResponse(response){
-    
-    alert(
-      `the weather in ${response.data.name} is ${response.data.main.temp} °C`
-    );
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+              autoFocuson
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100 "
+            />
+          </div>
+        </div>
+      </form>
+
+      <h1>paris</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+            <div className="clearfix mt-3">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly cloudy "
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>precipitation:15%</li>
+            <li>Humidity:72%</li>
+            <li>Wind:13KM/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
-let apiKey = "2672779dd04931dc0259289fbe9db665";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-axios.get(apiUrl).then(handleResponse);
-    return (
-      <Puff
-        height="80"
-        width="80"
-        radius={1}
-        color="orange"
-        ariaLabel="puff-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
-    );
- }
